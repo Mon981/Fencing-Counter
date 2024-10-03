488474be82b95ca044f78ed973aced97ebb9c19c
Mon981 <alwaysblue.marketing@gmail.com>
2024-10-03 03:32:15 +0200

    Total time set to 3 minutes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let totalB = 0
 document.getElementById("counterA").textContent = totalA
 document.getElementById("counterB").textContent = totalB
 let running = false;
-let time = (1*60*1000)-1000
+let time = (3*60*1000)-1000
 let updatedTime
 let timerEL = document.getElementById("timer")
 
@@ -50,7 +50,7 @@ function reset(){
     document.getElementById("counterA").textContent= totalA;
     document.getElementById("counterB").textContent= totalB;
     clearInterval(updatedTime)
-    time = (1*60*1000)-1000
+    time = (3*60*1000)-1000
     timerEL.textContent = "START"
     running = false
 }
@@ -58,29 +58,21 @@ function reset(){
 function startTimer() {    
 
     if (time > 0) {
-        // Calcular minutos y segundos
-        let minutes = Math.floor(time / 1000 / 60);
-        let seconds = Math.floor((time / 1000) % 60);
-
-        // Asegurarse de que los segundos siempre tengan dos dígitos
-        seconds = seconds < 10 ? '0' + seconds : seconds;
-
-        // Actualizar el contenido del temporizador
-        timerEL.textContent = `0${minutes} : ${seconds}`;
-
-        // Restar 1000 milisegundos (1 segundo) al tiempo
+        let minutes = Math.floor(time / 1000 / 60)
+        let seconds = Math.floor((time / 1000) % 60)
+        seconds = seconds < 10 ? '0' + seconds : seconds
+        timerEL.textContent = `0${minutes} : ${seconds}`        
         time= time - 1000;
     } else {
-        clearInterval(updatedTime); // Detener el temporizador cuando llegue a 0
-        timerEL.textContent = "Finish"; // Mostrar "Finish" cuando termine
+        clearInterval(updatedTime)
+        timerEL.textContent = "Finish"
     }
 }
 
 function updateTimer() {
     if (!running){
-        // timerEL.textContent= "03 : 00"
-    running = true;
-    updatedTime = setInterval(startTimer, 1000); // Llama a startTimer cada segundo}    
+    running = true
+    updatedTime = setInterval(startTimer, 1000) 
     } else{
         clearInterval(updatedTime)
         running = false
